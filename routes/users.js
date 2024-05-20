887c28d923996508115d7ccf3a43da1d387c0baa
Mael <maeldjedid@gmail.com>
2024-05-20 12:10:08 +0200

    q

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkbody");
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["firstName", "lastName", "email", "password", "role", "city"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({
+    mail: { $regex: new RegExp(req.body.mail, "i") },
+  }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        city: req.body.city,
+        role: req.body.role,
+        password: hash,
+        token: uid2(32),
+      });
+
+      newUser.save().then((newDoc) => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
+});
+
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({
+        result: true,
+        token: data.token,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+      });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
 });
 
 module.exports = router;
