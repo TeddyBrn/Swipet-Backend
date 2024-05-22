d877224807adbe0bb9a11cfbdf35f2d637309338
Hugo <hugo.bertin.scv@gmail.com>
2024-05-22 12:55:31 +0200

    route-match

--- routes/matchs.js
+++ routes/matchs.js
@@ -14,4 +14,26 @@ router.get('/matchs', (req, res) => {
       });
   });
   
+  router.post('/match', async (req, res) => {
+    const { user_id, petsitter_id } = req.body;
+
+    const existingMatch = await Match.findOne({ user_id, petsitter_id });
+    if (existingMatch) {
+        return res.status(400).json({ message: 'Match already exists.' });
+    }
+
+    const newMatch = new Match({ user_id, petsitter_id });
+    await newMatch.save();
+    res.status(201).json(newMatch);
+});
+
+router.get('/matches/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const matches = await Match.find({ $or: [{ user_id: userId }, { petsitter_id: userId }] })
+      .populate('user_id')
+      .populate('petsitter_id')
+      .populate('messages.sender');
+  res.json(matches);
+});
+
   module.exports = router;
