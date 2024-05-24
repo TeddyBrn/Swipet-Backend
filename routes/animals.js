3b10f2102956684b3c2b536ce848bfb5f9240982
Hugo <hugo.bertin.scv@gmail.com>
2024-05-24 10:15:56 +0200

    réparation-routeaddanimal

--- routes/animals.js
+++ routes/animals.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 require("../models/connection");
-const  Profil = require("../models/profils");
-const Animal = require("../models/animals");
+const  Profil  = require("../models/profils");
+const Animal  = require("../models/animals");
 const cloudinary = require("cloudinary").v2;
 const uniqid = require("uniqid");
 const fs = require("fs");
@@ -11,13 +11,13 @@ const fs = require("fs");
 router.post('/addanimal/:token', async (req, res) => {
     try {
       const { name, birthDate, animalType, gender, bio, detail } = req.body;
+      
       const userId = req.params.token;
   
       const photoPath = `./tmp/${uniqid()}.jpg`;
       await req.files.photoUrl.mv(photoPath);
       const cloudinaryResponse = await cloudinary.uploader.upload(photoPath);
       const photoUrl = cloudinaryResponse.secure_url;
-      console.log(photoUrl)
       fs.unlinkSync(photoPath);
   
       // Création d'un nouvel animal
@@ -42,7 +42,7 @@ router.post('/addanimal/:token', async (req, res) => {
       if (!Profil) {
         return res.status(404).json({ success: false, message: 'Profil utilisateur non trouvé' });
       }
-      res.status(201).json({ success: true, newAnimal: savedAnimal });
+      res.status(201).json({ success: true, animalId: savedAnimal._id });
     } catch (error) {
       res.status(500).json({ success: false, error: error.message });
     }
