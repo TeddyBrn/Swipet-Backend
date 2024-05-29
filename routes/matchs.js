33ec1252c80265973f518a9a4de112227fd31a6d
Teddy <teddy.buran33@gmail.com>
2024-05-29 15:49:33 +0200

    e

--- routes/matchs.js
+++ routes/matchs.js
@@ -1,14 +1,15 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-require("../models/connection");
-const { checkBody } = require("../modules/checkbody");
-const {Profil} = require("../models/profils");
-const {Match} = require('../models/matchs')
+require('../models/connection');
+const { checkBody } = require('../modules/checkbody');
+const { Profil } = require('../models/profils');
+const { Match } = require('../models/matchs');
 
 // Get all matchs
-router.get("/matchs", (req, res) => {
-  Match.find().then((data) => {
+router.get('/matchs', (req, res) => {
+  Match.find()
+    .then((data) => {
       res.json({ result: true, data });
     })
     .catch((error) => {
@@ -17,68 +18,73 @@ router.get("/matchs", (req, res) => {
 });
 
 //Récuperer tous les matchs d'un user
-router.get("/:userToken", async (req, res) => {
-
+router.get('/:userToken', async (req, res) => {
   const userToken = req.params.userToken;
-  const user = await Profil.findOne({token: userToken});
-  console.log(user)
+  const user = await Profil.findOne({ token: userToken });
+  console.log(user);
   const userId = user._id;
-  const matches = await Match.find({ user_id: userId
+  const matches = await Match.find({
+    user_id: userId
     // $or: [{ user_id: userId }, { petsitter_id: userId }],
   })
-    .populate("user_id")
-    .populate("petsitter_id")
-    .populate("messages");
-  res.json({result: true, matches: matches});
+    .populate('user_id')
+    .populate('petsitter_id')
+    .populate('messages');
+  res.json({ result: true, matches: matches });
   // console.log(matches)
 });
 
-router.post("/like/:userToken/:likedUserId", async (req, res) => {
+router.post('/like/:userToken/:likedUserId', async (req, res) => {
   const userToken = req.params.userToken;
-  const likedUserId = req.params.likedUserId
-console.log(userToken)
-  console.log(likedUserId, userToken)
-  
-  const user = await Profil.findOne({token: userToken});
+  const likedUserId = req.params.likedUserId;
+  console.log(userToken);
+  console.log(likedUserId, userToken);
+
+  const user = await Profil.findOne({ token: userToken });
   const userId = user._id;
   const likedUser = await Profil.findById(likedUserId);
-  console.log('console', user)
-  console.log('log', likedUser)
+  console.log('console', user);
+  console.log('log', likedUser);
 
   // un if pour éviter de liker deux fois la meme personne
   if (!user.like.includes(likedUserId)) {
-     await Profil.updateOne({_id: userId}, { $push: { like: likedUserId } })
-     await Profil.updateOne({_id: likedUserId}, { $push: { likeReceived: userId } })
-   } 
+    await Profil.updateOne({ _id: userId }, { $push: { like: likedUserId } });
+    await Profil.updateOne(
+      { _id: likedUserId },
+      { $push: { likeReceived: userId } }
+    );
+  }
   // else {
   // //   res.json({result: false, message: 'like already exist'})
   // // };
- 
 
   user.like.push(likedUserId);
-  likedUser.likeReceived.push(userId)
+  likedUser.likeReceived.push(userId);
   // console.log(user.like)
 
   // création de match
-  if (likedUser.like.includes(userId) && likedUser.likeReceived.includes(userId) ) {
+  if (
+    likedUser.like.includes(userId) &&
+    likedUser.likeReceived.includes(userId)
+  ) {
     // if (Match.find({user_id: userId, petsitter_id: likedUserId})) {
     //   res.json({result: false, message: 'match already exist'})
     //     } else {
-        // match = true;
-          const newMatch = new Match({
-            user_id: userId,
-            petsitter_id: likedUserId,
-            messages: [],
-            proposal: [],
-          })
-          
-          newMatch.save().then((newDoc) => {
-            // console.log('console log newMatch', newDoc)
-            res.json({ result: true, message: 'new match created!' });
-          });
-        // }
+    // match = true;
+    const newMatch = new Match({
+      user_id: userId,
+      petsitter_id: likedUserId,
+      messages: [],
+      proposal: []
+    });
+
+    newMatch.save().then((newDoc) => {
+      // console.log('console log newMatch', newDoc)
+      res.json({ result: true, message: 'new match created!' });
+    });
+    // }
   } else {
-    res.json({result: true, message: 'like created'})
+    res.json({ result: true, message: 'like created' });
   }
 });
 
@@ -103,19 +109,9 @@ console.log(userToken)
 
 //   })
 
-//   new Match = 
-
-
-
-
+//   new Match =
 
 // } )
-
-
-
-
-
-
 
 // router.post("/match", async (req, res) => {
 //   const { user_id, petsitter_id } = req.body;
@@ -130,11 +126,7 @@ console.log(userToken)
 //   res.status(201).json(newMatch);
 // });
 
-
-
 // NE PAS SUPPRIMER LA ROUTE DU DESSOUS ELLE EST EN CHANTIER
-
-
 
 //     user.likeReceived.push(likedUserId);
 //     likedUser.likeReceived.push(userId);
